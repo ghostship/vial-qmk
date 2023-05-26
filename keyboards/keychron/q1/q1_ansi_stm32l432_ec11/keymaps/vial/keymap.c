51bad9119f491dcbf70e6fd3d7157265e8c80b38
Scott Keller <scott.keller@inspiringapps.com>
2023-05-25 18:09:20 -0600

    Custom firmware mods

--- keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/vial/keymap.c
+++ keyboards/keychron/q1/q1_ansi_stm32l432_ec11/keymaps/vial/keymap.c
@@ -16,6 +16,18 @@
 
 #include QMK_KEYBOARD_H
 
+// For STT_CUSTOM_GRADIENT
+HSV stt_gradient_0 = {205, 250, 255};
+HSV stt_gradient_100 = {140, 215, 125};
+bool stt_reflected_gradient = false;
+uint8_t stt_gp_i = 0;
+
+typedef struct {
+    HSV gradient_0;
+    HSV gradient_1;
+    bool reflected;
+} STT_CUSTOM_PRESETS;
+
 // clang-format off
 
 enum layers{
@@ -33,7 +45,22 @@ enum custom_keycodes {
     KC_LCMMD,
     KC_RCMMD,
     KC_TASK_VIEW,
-    KC_FILE_EXPLORER
+    KC_FILE_EXPLORER,
+    STT_G0_HUI,                 //Custom gradient color 1 hue increase
+    STT_G0_HUD,                 //Custom gradient color 1 hue decrease
+    STT_G0_SAI,                 //Custom gradient color 1 saturation increase
+    STT_G0_SAD,                 //Custom gradient color 1 saturation decrease
+    STT_G0_VAI,                 //Custom gradient color 1 value increase
+    STT_G0_VAD,                 //Custom gradient color 1 value decrease
+    STT_G100_HUI,               //Custom gradient color 2 hue increase
+    STT_G100_HUD,               //Custom gradient color 2 hue decrease
+    STT_G100_SAI,               //Custom gradient color 2 saturation increase
+    STT_G100_SAD,               //Custom gradient color 2 saturation decrease
+    STT_G100_VAI,               //Custom gradient color 2 value increase
+    STT_G100_VAD,               //Custom gradient color 2 value decrease
+    STT_GRADIENT_PRESETS,       //Gradient presets
+    STT_REFLECTED_GRADIENT,     //Toggle between linear and reflected gradient
+    STT_GRADIENT_FLIP,          //Flip the gradient colors
 };
 
 #define KC_MCTL KC_MISSION_CONTROL
@@ -85,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                                                             RGB_VAD, RGB_VAI,
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  STT_GRADIENT_PRESETS,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
         _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
@@ -136,6 +163,20 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint8_t color_adj_step = 5;
+    STT_CUSTOM_PRESETS stt_gradient_presets[] = {
+        {{205, 250, 255}, {140, 215, 125}, false },
+        {{41, 255, 255}, {233, 245, 255}, false },
+        {{45, 245, 155}, {160, 255, 80}, false },
+        {{173, 245, 40}, {41, 255, 205}, true },
+        {{32, 255, 165}, {217, 185, 70}, false },
+        {{240, 255, 145}, {115, 255, 245}, true },
+        {{118, 255, 255}, {242, 255, 255}, false },
+        {{118, 255, 255}, {242, 255, 255}, false },
+        {{212, 0, 0}, {223, 235, 165}, true },
+    };
+    uint8_t stt_gp_length = sizeof(stt_gradient_presets)/sizeof(stt_gradient_presets[0]);
+
     switch (keycode) {
         case KC_MISSION_CONTROL:
             if (record->event.pressed) {
@@ -173,6 +214,101 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;  // Skip all further processing of this key
+        case STT_G0_HUI:
+            if (record->event.pressed) {
+                stt_gradient_0.h += color_adj_step;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G0_HUD:
+            if (record->event.pressed) {
+                stt_gradient_0.h -= color_adj_step;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G0_SAI:
+            if (record->event.pressed) {
+                stt_gradient_0.s = (stt_gradient_0.s + color_adj_step * 2 <= 255) ? stt_gradient_0.s + color_adj_step * 2 : 255;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G0_SAD:
+            if (record->event.pressed) {
+                stt_gradient_0.s = (stt_gradient_0.s - color_adj_step * 2 >= 0) ? stt_gradient_0.s - color_adj_step * 2 : 0;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G0_VAI:
+            if (record->event.pressed) {
+                stt_gradient_0.v = (stt_gradient_0.v + color_adj_step * 2 <= 255) ? stt_gradient_0.v + color_adj_step * 2 : 255;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G0_VAD:
+            if (record->event.pressed) {
+                stt_gradient_0.v = (stt_gradient_0.v - color_adj_step * 2 >= 0) ? stt_gradient_0.v - color_adj_step * 2 : 0;
+                dprintf("Gradient 0 HSV: %d, %d, %d\n", stt_gradient_0.h, stt_gradient_0.s, stt_gradient_0.v);
+            }
+            return false;
+        case STT_G100_HUI:
+            if (record->event.pressed) {
+                stt_gradient_100.h += color_adj_step;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_G100_HUD:
+            if (record->event.pressed) {
+                stt_gradient_100.h -= color_adj_step;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_G100_SAI:
+            if (record->event.pressed) {
+                stt_gradient_100.s = (stt_gradient_100.s + color_adj_step * 2 <= 255) ? stt_gradient_100.s + color_adj_step * 2 : 255;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_G100_SAD:
+            if (record->event.pressed) {
+                stt_gradient_100.s = (stt_gradient_100.s - color_adj_step * 2 >= 0) ? stt_gradient_100.s - color_adj_step * 2 : 0;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_G100_VAI:
+            if (record->event.pressed) {
+                stt_gradient_100.v = (stt_gradient_100.v + color_adj_step * 2 <= 255) ? stt_gradient_100.v + color_adj_step * 2 : 255;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_G100_VAD:
+            if (record->event.pressed) {
+                stt_gradient_100.v = (stt_gradient_100.v - color_adj_step * 2 >= 0) ? stt_gradient_100.v - color_adj_step * 2 : 0;
+                dprintf("Gradient 100 HSV: %d, %d, %d\n", stt_gradient_100.h, stt_gradient_100.s, stt_gradient_100.v);
+            }
+            return false;
+        case STT_GRADIENT_PRESETS:
+            if (record->event.pressed) {
+                stt_gp_i = (stt_gp_i + stt_gp_length ) % stt_gp_length;
+
+                stt_gradient_0 = stt_gradient_presets[stt_gp_i].gradient_0;
+                stt_gradient_100 = stt_gradient_presets[stt_gp_i].gradient_1;
+                stt_reflected_gradient = stt_gradient_presets[stt_gp_i].reflected;
+
+                stt_gp_i += 1;
+            }
+            return false;
+        case STT_REFLECTED_GRADIENT:
+            if (record->event.pressed) {
+                stt_reflected_gradient = !stt_reflected_gradient;
+            }
+            return false;
+        case STT_GRADIENT_FLIP:
+            if (record->event.pressed) {
+                HSV temp_color = stt_gradient_0;
+                stt_gradient_0 = stt_gradient_100;
+                stt_gradient_100 = temp_color;
+            }
+            return false;
         default:
             return true;   // Process all other keycodes normally
     }
